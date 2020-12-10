bac0fa25334d0e2c4664f1546b82f1268f113c64
ajdev7 <aaron.dev1124@gmail.com>
2020-12-11 05:39:15 +0800

    First version done.

--- app/src/main/java/dev/marcosfarias/terpdex/utils/TerpmonColorUtil.kt
+++ app/src/main/java/dev/marcosfarias/terpdex/utils/TerpmonColorUtil.kt
@@ -12,11 +12,13 @@ class TerpmonColorUtil(var context: Context) {
     fun getTerpmonColor(typeOfTerpmon: List<String>?): Int {
         val type = typeOfTerpmon?.getOrNull(0)
         val color = when (type?.toLowerCase()) {
-            "grass", "bug" -> R.color.lightTeal
-            "fire" -> R.color.lightRed
+            "caryophyllene" -> R.color.lightRed
+            "pinene" -> R.color.lightTeal
+            "ocimene" -> R.color.lightPurple
+            "nerolidol" -> R.color.yellow
+
             "water", "fighting", "normal" -> R.color.lightBlue
             "electric", "psychic" -> R.color.lightYellow
-            "poison", "ghost" -> R.color.lightPurple
             "ground", "rock" -> R.color.lightBrown
             "dark" -> R.color.black
             else -> R.color.lightBlue
